1c4c56f93b6d735596eec0807ba52131b44a3647
mglienecke <michael@glienecke.de>
2025-02-25 12:34:30 +0000

    Prepare for class

--- src/main/java/uk/ac/ed/inf/kafkasamples/StockSymbolConsumer.java
+++ src/main/java/uk/ac/ed/inf/kafkasamples/StockSymbolConsumer.java
@@ -1,7 +1,5 @@
 package uk.ac.ed.inf.kafkasamples;
 
-
-
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -22,10 +20,13 @@ import java.util.Properties;
 public class StockSymbolConsumer {
 
     /**
-     * launch the consumer
-     * @param args
-     * @throws IOException
-     * @throws InterruptedException
+     * The entry point of the application.
+     * This method initializes the StockSymbolConsumer and processes symbols
+     * based on the provided configuration file.
+     *
+     * @param args Command-line arguments where the first argument must be the path to the configuration file.
+     * @throws IOException If there is an issue reading the configuration file.
+     * @throws InterruptedException If the thread executing the method is interrupted during execution.
      */
     public static void main(String[] args) throws IOException, InterruptedException {
         if (args.length != 1) {
@@ -38,7 +39,17 @@ public class StockSymbolConsumer {
     }
 
 
-    // We'll reuse this function to load properties from the Consumer as well
+    /**
+     * Loads configuration properties from a file.
+     *
+     * This method reads properties from the specified configuration file and
+     * returns a {@link Properties} object containing the key-value pairs from the file.
+     * If the file does not exist, an {@link IOException} is thrown.
+     *
+     * @param configFile The path to the configuration file to be loaded.
+     * @return A {@link Properties} object containing the configuration key-value pairs.
+     * @throws IOException If the specified configuration file cannot be found or read.
+     */
     public static Properties loadConfig(final String configFile) throws IOException {
         if (!Files.exists(Paths.get(configFile))) {
             throw new IOException(configFile + " not found.");
@@ -54,10 +65,15 @@ public class StockSymbolConsumer {
     public final String KafkaTopicConfig = "kafka.topic";
 
     /**
+     * Processes stock symbol data from a Kafka topic based on the provided configuration file.
      *
-     * @param configFileName
-     * @throws IOException
-     * @throws InterruptedException
+     * The method reads configurations, sets up a Kafka consumer, subscribes to the defined topic,
+     * and continuously polls messages. It updates and logs the symbol values for known stock symbols based on the configuration.
+     * Unknown symbols are also logged. The method runs in an infinite loop and periodically pauses for a short duration.
+     *
+     * @param configFileName The path to the configuration file that contains Kafka and stock symbol settings.
+     * @throws IOException If there are issues reading the configuration file.
+     * @throws InterruptedException If the thread executing the method is interrupted.
      */
     private void process(String configFileName) throws IOException, InterruptedException {
         Properties kafkaPros = StockSymbolConsumer.loadConfig(configFileName);
